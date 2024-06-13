3e62778167595692c621f39f15458bd52a3a1e04
Hossam-Elsonbaty <armaggg3@gmail.com>
2024-06-13 11:18:13 +0300

    menu

--- uppharma/src/Components/Navbar.jsx
+++ uppharma/src/Components/Navbar.jsx
@@ -1,7 +1,57 @@
-import React from 'react';
+import React,{useState} from 'react';
 import logo from '../Images/logo192.png';
+import Menu from './Menu';
 import { FaShoppingCart, FaHeart, FaUser, FaSearch } from 'react-icons/fa';
 export default function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+  const menuItems = [
+    {
+      name: 'قسم العروض',
+    },
+    {
+      name: ' قسم العطور',
+    },
+    {
+      name: 'قسم المكملات الغذائية',
+      children: [
+        {
+          name: 'مكملات الفيتامينات ',
+          children: [
+            { name: 'المكملات الرياضية ' },
+            { name: 'مكملات الشعر' },
+          ],
+        },
+        {
+          name: 'مكملات الاعشاب  ',
+        },
+      ],
+    },
+    {
+      name: 'قسم الرعاية الصيدلانية  ',
+      children: [
+        {
+          name: 'قسم البنادول  ',
+        },
+        {
+          name: ' قسم البرد والأنفلونزا  ',
+        },
+      ],
+    },
+    {
+      name: 'قسم العناية بالأطفال  ',
+      children: [
+        {
+          name: 'صيدلية الطفل  ',
+        },
+        {
+          name: ' قسم الجلدية للأطفال  ',
+        },
+      ],
+    },
+  ];
   return (
     <header className='Nav-header'>
       <nav className="navbar">
@@ -22,12 +72,13 @@ export default function Navbar() {
           <div className="logo">
             <img src={logo} alt="err"/>
           </div>
-          <div className="nav-bars">
+          <div className="nav-bars" onClick={toggleMenu}>
             <span className='bar'></span>
             <span className='bar'></span>
             <span className='bar'></span>
           </div>
         </div>
+        <Menu items={menuItems} isOpen={isMenuOpen}/>
       </nav>
     </header>
   )
