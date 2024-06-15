5481736efb87a1bd754b8d24aeaad42e8287422e
Hossam-Elsonbaty <armaggg3@gmail.com>
2024-06-15 19:36:35 +0300

    pagination

--- uppharma/src/Pages/Section.jsx
+++ uppharma/src/Pages/Section.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { FaArrowDown } from "react-icons/fa";
 import Product from '../Components/Product';
+import Footer from '../Components/Footer';
+import { Pagination } from 'antd';
 export default function Section() {
+  const [first, setFirst] = useState(0);
+  const [rows, setRows] = useState(10);
+  const onPageChange = (event) => {
+    setFirst(event.first);
+    setRows(event.rows);
+  };
   return (
     <>
       <main className='section-main'>
@@ -13,8 +21,11 @@ export default function Section() {
           </div>
         </div>
         <Product></Product>
+        <div className='pagination'>
+          <Pagination defaultCurrent={1} total={50} />
+        </div>
+        <Footer></Footer>
       </main>
-      
     </>
   )
 }
