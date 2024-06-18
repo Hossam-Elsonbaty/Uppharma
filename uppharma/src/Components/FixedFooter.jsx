01760009e0414db68a6e93136b153d9ff77acb9a
Hossam-Elsonbaty <armaggg3@gmail.com>
2024-06-18 20:42:22 +0300

    edits

--- uppharma/src/Components/FixedFooter.jsx
+++ uppharma/src/Components/FixedFooter.jsx
@@ -4,11 +4,12 @@ import { CiPhone } from "react-icons/ci";
 import { FaWhatsapp } from "react-icons/fa";
 import { CiHeart } from "react-icons/ci";
 import { CiUser } from "react-icons/ci";
+import { PiWhatsappLogoLight } from "react-icons/pi";
 export default function FixedFooter() {
   return (
     <div className='fixed-footer'>
       <div className='col'><CiPhone /></div>
-      <div className='col'><FaWhatsapp /></div>
+      <div className='col'><PiWhatsappLogoLight /></div>
       <div className='col'>
         <div className='cart-items-number'>
           <span>0</span>
