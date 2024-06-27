e7459ae59be908e3774b54be101f8b158b64e734
Hossam-Elsonbaty <armaggg3@gmail.com>
2024-06-27 16:21:51 +0300

    order info

--- uppharma/src/Components/FixedFooter.jsx
+++ uppharma/src/Components/FixedFooter.jsx
@@ -6,21 +6,26 @@ import { CiHeart } from "react-icons/ci";
 import { CiUser } from "react-icons/ci";
 import { PiWhatsappLogoLight } from "react-icons/pi";
 import { Link } from 'react-router-dom';
+import { FiShoppingCart } from "react-icons/fi";
+import { MdPhone } from "react-icons/md";
+import { BsWhatsapp } from "react-icons/bs";
+import { FaRegHeart } from "react-icons/fa";
+import { HiOutlineUserCircle } from "react-icons/hi";
 export default function FixedFooter() {
   return (
     <div className='fixed-footer'>
-      <div className='col'><CiPhone /></div>
-      <div className='col'><PiWhatsappLogoLight /></div>
+      <div className='col'><MdPhone  /></div>
+      <div className='col'><FaRegHeart  /></div>
       <div className='col'>
         <Link to="/my-cart">
           <div className='cart-items-number'>
-            <span>0</span>
+            <span>2</span>
           </div>
-          <CiShoppingCart />        
+          <FiShoppingCart  />        
         </Link>
       </div>
-      <div className='col'><CiHeart /></div>
-      <div className='col'><CiUser /></div>
+      <div className='col'><BsWhatsapp  /></div>
+      <div className='col'><HiOutlineUserCircle   /></div>
     </div>
   )
 }
--- uppharma/src/Components/PaymentMethods.jsx
+++ uppharma/src/Components/PaymentMethods.jsx
@@ -3,56 +3,6 @@ import { CiCreditCard1 } from "react-icons/ci";
 
 const PaymentMethods = () => {
   return <>
-    <div className="total-price">
-      <div className="element">
-        <div className='col'>
-          <h3>قيمة المنتجات </h3>
-          <span>د.ك<span>1.5</span></span>
-        </div>
-        <div className='col'>
-          <h3>الخصم</h3>
-          <span>د.ك<span>5</span></span>
-        </div>
-        <div className='col'>
-          <h3>تكاليف الشحن</h3>
-          <span>د.ك<span>5</span></span>
-        </div>
-        <div className='col'>
-          <h3>المجموع النهائي</h3>
-          <span>د.ك<span>5</span></span>
-        </div>
-      </div>
-      <div className="element">
-        <div className='col'>
-          <input type="text" name="" id="" placeholder='كود الخصم' />
-        </div>
-        <div className='col'>
-          <button>
-            تحصيل
-          </button>
-        </div>
-        <div className='col'>
-          <span>
-            إجمالي الخصم:
-          </span>
-          <span>
-            د.ك<span>5</span>
-          </span>
-        </div>
-      </div>
-      <div className="element">
-        <div className='col'>
-          <h3 className='price'>8</h3>
-          <span>د.ك</span>
-        </div>
-        <div className='col'>
-          <button>
-            <span>الدفع</span>
-            <CiCreditCard1 />
-          </button>
-        </div>
-      </div>
-    </div>
   </>;
 };
 export default PaymentMethods;
