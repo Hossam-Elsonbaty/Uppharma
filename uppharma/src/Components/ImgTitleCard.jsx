e8de7a70eab81963dae29bd6e550e27734240437
ahmadhossam24 <70440432+ahmadhossam24@users.noreply.github.com>
2024-11-29 04:32:45 +0200

    categories links

--- uppharma/src/Components/ImgTitleCard.jsx
+++ uppharma/src/Components/ImgTitleCard.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function ImgTitleCard({status}) {
+  const navigate = useNavigate();
   return (
-          <div className="card">
+          <div className="card"  onClick={() => navigate(status.link)}>
             <div className="health-state-img-container">
               <img src={status.image} alt="" />
             </div>
